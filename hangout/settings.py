42ef305c902bd77bfc7f812bf82acd9e6a218e90
Stephen Jakku <uttamjakku96@gmail.com>
2022-06-18 22:00:09 -0700

    added .env file

--- hangout/settings.py
+++ hangout/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zm^0#%z)tuj0=jtn$w9a=1)yxq7#n)32*8-33ds=dz!n!p3tec'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
