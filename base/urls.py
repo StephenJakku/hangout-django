bb0c8ea14ff9ba23c703b3d5a1df1171973c0011
Stephen Jakku <uttamjakku96@gmail.com>
2022-07-06 21:31:49 -0700

    Linked database to front end, added create room form

--- base/urls.py
+++ base/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.home, name='home'),
-               path('room/<str:pk>/', views.room, name='room'), ]
+               path('room/<str:pk>/', views.room, name='room'),
+               path('create-room', views.createRoom, name="create-room")]
